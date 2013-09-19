97360d2cd3d025d8dc1ba0735d18d36c2f9d5c29
Shane Fu <iPhone@Xiaoxies-iMac.local>
2013-09-19 10:45:32 -0700

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Shane Fu on 9/19/13.
+//  Copyright (c) 2013 Shane Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Shane Fu on 9/19/13.
+//  Copyright (c) 2013 Shane Fu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CalculatorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([CalculatorAppDelegate class]));
+	}
+}
