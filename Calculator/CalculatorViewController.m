2707339e3e488cded7c6d027a78bc8ee9786cb94
Shane Fu <iPhone@Xiaoxies-iMac.local>
2013-09-20 11:12:53 -0700

    get the cursor working

--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -11,8 +11,10 @@
 
 @interface CalculatorViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *resultDisplay;
-@property (weak, nonatomic) IBOutlet UILabel *functionDisplay;
+@property (weak, nonatomic) IBOutlet UITextField *functionDisplay;
 @property (nonatomic) BOOL userIsInTheMiddleOfEnteringADigit;
+@property (nonatomic) BOOL userHasEnteredAnOpration;
+@property (nonatomic, strong) UITextPosition *cursorNewPosition;
 
 @property (nonatomic, strong) CalculatorBrain *brain;
 
@@ -20,6 +22,15 @@
 
 @implementation CalculatorViewController
 @synthesize brain = _brain;
+@synthesize cursorNewPosition = _cursorNewPosition;
+
+-(UITextPosition *)cusorNewPosition
+{
+	if (_cursorNewPosition) {
+		_cursorNewPosition = [[UITextPosition alloc] init];
+	}
+	return _cursorNewPosition;
+}
 
 -(CalculatorBrain *)brain
 {
@@ -33,6 +44,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	self.functionDisplay.inputView = [[UIView alloc] initWithFrame:CGRectZero];
+	self.functionDisplay.text = @"nihaola";
+	[self.functionDisplay becomeFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
@@ -40,6 +54,7 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 - (IBAction)digitPressed:(UIButton *)sender {
 	if (self.userIsInTheMiddleOfEnteringADigit) {
 		self.resultDisplay.text = [self.resultDisplay.text stringByAppendingFormat:@"%@",[sender currentTitle]];
@@ -48,19 +63,44 @@
 		self.userIsInTheMiddleOfEnteringADigit = YES;
 	}
 }
+
 - (IBAction)operationPressed:(UIButton *)sender {
-	if (self.userIsInTheMiddleOfEnteringADigit)
+	if (!self.userHasEnteredAnOpration)
 	{
-		[self enterPressed];
+		NSString *operation = [sender currentTitle];
+		double result = [self.brain performOperand:operation];
+		self.resultDisplay.text = [NSString stringWithFormat:@"%g",result];
 	}
-	
-	NSString *operation = [sender currentTitle];
-	double result = [self.brain performOperand:operation];
-	self.resultDisplay.text = [NSString stringWithFormat:@"%g",result];
-}
-- (IBAction)enterPressed {
-	self.userIsInTheMiddleOfEnteringADigit = NO;
-	[self.brain pushOperand:[self.resultDisplay.text doubleValue]];
 }
 
+//enterButton just run the program and update the result
+- (IBAction)enterPressed {
+}
+
+- (IBAction)moveCursor:(UIButton *)sender {
+	UITextPosition *endOfDocument = [self.functionDisplay endOfDocument];
+
+	UITextRange *selectionRange = self.functionDisplay.selectedTextRange;
+	UITextPosition *selectedStartPosition = selectionRange.start;
+	int cursorIndex = [self.functionDisplay offsetFromPosition:self.functionDisplay.endOfDocument toPosition:selectedStartPosition];
+	
+	if ([sender.currentTitle isEqualToString:@"->"]) {
+		// Calculate the new position, - for left and + for right
+		cursorIndex ++;
+		NSLog(@"cursorIndex = %d", cursorIndex);
+		self.cursorNewPosition = [self.functionDisplay positionFromPosition:endOfDocument offset:cursorIndex];
+		
+	}else{
+		// Calculate the new position, - for left and + for right
+		cursorIndex --;
+		NSLog(@"cursorIndex = %d", cursorIndex);
+		self.cursorNewPosition = [self.functionDisplay positionFromPosition:endOfDocument offset:cursorIndex];
+	}
+	
+	UITextRange *newRange = [self.functionDisplay textRangeFromPosition:self.cursorNewPosition toPosition:self.cursorNewPosition];
+	
+	// Set new range
+	[self.functionDisplay setSelectedTextRange:newRange];
+	
+}
 @end
